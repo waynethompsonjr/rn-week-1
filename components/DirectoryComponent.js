d88a04945c57634fdb813413329d4427a15a7386
Wayne Thompson <wayne.thompsonjr@gmail.com>
2022-04-07 21:26:27 -0500

    copy pasted lesson code because I was getting an error that I could not decipher

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
-import { CAMPSITES } from './shared/campsites';
+import { CAMPSITES } from '../shared/campsites';
 
 class Directory extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -27,7 +28,7 @@ class Directory extends Component {
                 />
             );
         };
-    
+
         return (
             <FlatList
                 data={this.state.campsites}
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import Constants from 'expo-constants';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
@@ -9,7 +10,7 @@ const DirectoryNavigator = createStackNavigator(
     {
         Directory: { screen: Directory },
         CampsiteInfo: { screen: CampsiteInfo }
-    },
+    }, 
     {
         initialRouteName: 'Directory',
         defaultNavigationOptions: {
@@ -29,9 +30,10 @@ const AppNavigator = createAppContainer(DirectoryNavigator);
 class Main extends Component {
     render() {
         return (
-            <View style={{
-                flex: 1,
-                paddingTop: Platform.OS === 'ios' ? 0 : ConstantSourceNode.statusBarHeight
+            <View
+                style={{
+                    flex: 1,
+                    paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
             }}>
                 <AppNavigator />
             </View>
