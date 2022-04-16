ad1d94a7405dc1679348a734a908a7b94739e5ed
Wayne Thompson <wayne.thompsonjr@gmail.com>
2022-04-16 10:51:30 -0500

    completed week 1 task 3

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
-import { View, Platform } from 'react-native'
-import { ScrollView } from 'react-native-gesture-handler';
+import { Text, ScrollView, FlatList } from 'react-native';
+import { ListItem, Card } from 'react-native-elements';
+import PARTNERS from '../shared/partners';
 
 
-function RenderItem({item}) {
-    if (item) {
-        return (
-            <Card 
-                featuredTitle = { item.name }
-            >
-                <Text style={{margin: 10}}>
-                    { item.description }
-                </Text>
-            </Card>
-        );
-    }
-    return <View />;
+function Mission() {
+   
+    return (
+        <Card title={'Our Mission'}>
+            <Text style={{margin: 10}}>
+                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. 
+                We increase access to adventure for the public while promoting safe and respectful use of resources. 
+                The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. 
+                We also present a platform for campers to share reviews on campsites they have visited with each other.
+            </Text>
+        </Card>
+    );  
 }
 
-
 class About extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            partners: PARTNERS
+        };
+    }
  
     static navigationOptions = {
         title: 'About Us'
@@ -28,9 +32,28 @@ class About extends Component {
 
     render() {
 
+        const renderPartner = ({item}) => {
+            return(
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    leftAvatar={{source: require('./images/bootstrap-logo.png')}}
+                />
+            );
+        };
+
         return(
             <ScrollView>
-
+                <Mission />
+                <Card 
+                    title='Community Partners'
+                >
+                    <FlatList
+                        data={this.state.partners}
+                        renderItem={renderPartner}
+                        keyExtractor={item => item.id.toString()}
+                    />
+                </Card>
             </ScrollView>
         );
     }
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,23 +1,8 @@
 import React, { Component } from 'react';
-import { View, Platform, ScrollView } from 'react-native'
+import { Text, View, Platform, ScrollView } from 'react-native'
 //import { ScrollView } from 'react-native-gesture-handler'; // Where did this come from?
 import { Card } from 'react-native-elements';
 
-
-function RenderItem() {
-
-        return (
-            <Card wrapperStyle={{margin: 20}}
-                featuredTitle='Contact Information'
-            >
-                <Text>1 Nucamp Way</Text>
-                <Text>Seattle, WA 98001</Text>
-                <Text style={{marginBottom: 10}}>U.S.A.</Text>
-                <Text>Phone: 1-206-555-1234</Text>
-                <Text>Email: campsites@nucamp.co</Text>   
-            </Card>
-        );
-}
 
 class Contact extends Component {
 
@@ -28,7 +13,16 @@ class Contact extends Component {
     render() {
         return (
             <ScrollView>
-                <RenderItem />
+                <Card title={'Contact Information'}
+                      wrapperStyle={{margin: 20}}
+                >
+                    <Text>1 Nucamp Way</Text>
+                    <Text>Seattle, WA 98001</Text>
+                    <Text style={{marginBottom: 10}}>U.S.A.</Text>
+                    <Text>Phone: 1-206-555-1234</Text>
+                    <Text>Email: campsites@nucamp.co</Text>   
+                </Card>
+
             </ScrollView>
         );
     }
